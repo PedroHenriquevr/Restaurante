15994a064a115441fbe92158636e830585e3ad7d
PedroHenriquevr <pedrohvribeiro@gmail.com>
2023-08-02 08:59:27 -0300

    create react app

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function Cardapio() {
+    const [count, setCount] = useState()
+    const addValue() => () {
+        setCount + 1;
+    }
+    return(
+        <>
+        <h1>Pepe Delicinhas</h1>
+        <button onClick={}></button>
+        </>
+    )
+}
