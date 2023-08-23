c1351cc9c086e27c26ff6691bf8ea74154dc64b4
Pedro Henrique Vieira Ribeiro <pedrohvribeiro@gmail.com>
2023-08-23 09:00:18 -0300

    add: page Buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,17 @@
 import styles from './Cardapio.module.scss'
-import logo from '../../assets/logo_pepe.png'
+import logo from 'assets/logo_pepe.png'
 export default function Cardapio() {
 
     return(
         <main>
             <nav className={styles.menu}>
-                <img src={logo} className={styles.logo} />
+                <img src={logo} className={styles.logo} alt="logo" />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    Pepe Delicinhas
+                </div>
+            </header>
         </main>
     )
 }
