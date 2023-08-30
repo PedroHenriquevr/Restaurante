451fd1b6ee282f6e8c1a20df9bf99a0e26554828
PedroHenriquevr <pedrohvribeiro@gmail.com>
2023-08-30 08:58:41 -0300

    add: styles

--- src/pages/Buscador/index.tsx
+++ src/pages/Buscador/index.tsx
@@ -1,3 +1,18 @@
-export default function Buscador() {
-    return
+import style from './Buscador.module.scss'
+
+type props = {
+    busca: string,
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Buscador({busca, setBusca}: props) {
+
+    return(
+        <div className={style.buscador}>
+            <input className={style.input} value={busca}
+            onChange={(evento) => setBusca(evento.target.value)}
+            type='text'
+            />
+        </div>
+    )
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import styles from './Cardapio.module.scss'
 import logo from 'assets/logo_pepe.png'
+import Buscador from 'pages/Buscador';
 export default function Cardapio() {
-
+    const [busca, setBusca] = useState("");
     return(
         <main>
             <nav className={styles.menu}>
@@ -12,6 +14,10 @@ export default function Cardapio() {
                     Pepe Delicinhas
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3>Cardapio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+            </section>
         </main>
     )
 }
